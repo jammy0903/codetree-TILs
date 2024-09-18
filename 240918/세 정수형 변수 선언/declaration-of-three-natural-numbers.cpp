21143f50f2d76f56a02de0f0f2228cb0a562af4d
kim so jeong <144465258+jammy0903@users.noreply.github.com>
2024-09-18 17:54:22 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240918/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a=7;
+    int b=23;
+
+    cout<<a<<" + "<<b<<" = "<<a+b;
+
+    return 0;
+}
