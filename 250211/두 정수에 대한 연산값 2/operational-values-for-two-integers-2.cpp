930222d8713927e071e1d23c7df895c237763c7e
kim so jeong <144465258+jammy0903@users.noreply.github.com>
2025-02-11 14:37:54 +0900

    Title: 두 정수에 대한 연산값 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250211/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a, b;
+
+int fun(int a, int b){
+    auto result = std::minmax(a,b);
+    int maxint = result.second*2;
+    int minint = result.first+10;
+
+    cout<<minint<<" "<<maxint;
+    return 0;
+}
+int main() {
+    cin >> a >> b;
+    fun(a,b);
+
+    return 0;
+
+
+}
