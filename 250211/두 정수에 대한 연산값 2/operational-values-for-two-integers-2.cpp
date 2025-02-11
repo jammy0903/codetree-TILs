afcb81d38d001579e42ec38fbe3a8b5923ce97c9
kim so jeong <144465258+jammy0903@users.noreply.github.com>
2025-02-11 14:57:50 +0900

    Title: 두 정수에 대한 연산값 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250211/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
+++ 250211/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include <algorithm>
+#include <utility>
 
 using namespace std;
 
 int a;
 int b;
 
-int fun(int A, int B){
+pair<int ,int > fun(int A, int B){
     int minint = std::min(A,B);
     int maxint = std::max(A,B);
     
     minint = minint+10;
     maxint = maxint*2;
-    cout<<minint<<" "<<maxint;
-    return 0;
+    //cout<<minint<<" "<<maxint;
+    return make_pair(minint, maxint); 
 }
 int main() {
     cin >> a >> b;
     if(a!=b){
-        fun(a,b);
+       pair<int , int> result = fun(a,b);
+       cout<<result.first<<" "<<result.second;
     }
     
     return 0;
