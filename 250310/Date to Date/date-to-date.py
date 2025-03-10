5b34efefea1913c440fe6b2cc58ea23aa6e4d8ab
kim so jeong <144465258+jammy0903@users.noreply.github.com>
2025-03-10 14:48:27 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250310/Date to Date/date-to-date.py
+++ 250310/Date to Date/date-to-date.py
@@ -1,37 +1,15 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-# Please write your code here.
-end = [0,31,28,31,30,31,30,31,30,31,31,30,31]
+# 각 월의 일 수 (2011년은 윤년이 아님)
+days_in_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-day_cnt = 0
+# 같은 달이면 단순히 날짜 차이 계산
+if m1 == m2:
+    print(d2 - d1 + 1)
+else:
+    # 첫 달의 남은 일수 + 마지막 달의 지난 일수 + 중간 달의 총 일수
+    total_days = (days_in_month[m1] - d1 + 1) + d2
+    for month in range(m1 + 1, m2):
+        total_days += days_in_month[month]
 
-
-while m1 <= m2 :
-
-
-    if m1 == m2 :
-        if d1 == d2 : #정답 
-            day_cnt +=1
-            break
-
-        elif d1 < d2 :
-            d1 +=1
-            day_cnt +=1
-            continue
-
-    elif m1 < m2  :
-        if d1 < end[m1] :
-            d1 +=1
-            day_cnt +=1
-            continue
-
-        elif d1 == end[m1] : #경계선
-            d1 = 1 # 1개월 올리자.
-            m1 +=1
-            day_cnt +=1
-            continue
-    
-print(day_cnt)
-
-
-
+    print(total_days)
