90fefd1e377941860399f9ad72cc54eacd291aa4
kim so jeong <144465258+jammy0903@users.noreply.github.com>
2026-03-30 10:53:43 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,19 +1,11 @@
-import sys 
+import sys
 input = sys.stdin.readline
 
-
-N ,K = map(int,input().split())
+N,K = map(int,input().split())
 coin=[int(input()) for _ in range(N)]
 coin.sort(reverse=True)
-
 cnt = 0
-
-for i in range(len(coin)):
-    if K == 0:
-        break
-        
-    cnt += K//coin[i] #동전개수 업데이트
-    K %= coin[i] #남은돈 업데이트
+for i in range(N):
+    cnt +=K // coin[i]
+    K %= coin[i]
 print(cnt)
-
-
